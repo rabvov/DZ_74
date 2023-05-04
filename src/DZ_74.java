3d84e7ca1e22716fbd662acb2d81a493212a6c84
rak15 <rak1529916@mail.ru>
2023-05-04 20:21:35 +0300

    1.1

--- src/DZ_74.java
+++ src/DZ_74.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class DZ_74 {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+public class DZ_74 {
+    public static void main(String[] args) throws FileNotFoundException {
+        String str;
+        int n = 5;
+        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\rak15\\IdeaProjects\\DZ_74\\File");
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                str = scanner.next();
+                if (str.length()>n){
+                    byte [] b = str.getBytes();
+                    fileOutputStream.write(b);
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
